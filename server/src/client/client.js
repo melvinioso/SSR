eb143357e9d220abfbb2ed9fd899a9f3bfcb9695
Matt McGavern <matt.mcgavern@gmail.com>
2022-02-28 18:12:17 -0600

    client side routing

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,7 +1,11 @@
 // Startup point for the client side application
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import Home from './components/Home';
+import Routes from './Routes';
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(
+  <BrowserRouter>{Routes}</BrowserRouter>,
+  document.querySelector('#root')
+);
