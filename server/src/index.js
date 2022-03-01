2f3de3ed2c5308fd8fdb85338df01e491ccf42e6
Matt McGavern <matt.mcgavern@gmail.com>
2022-03-01 17:08:20 -0600

    matchRoutes function

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
 
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -10,6 +12,8 @@ app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   const store = createStore();
+
+  console.log(matchRoutes(Routes, req.path));
 
   res.send(renderer(req, store));
 });
